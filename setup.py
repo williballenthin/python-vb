cff03de3daa063f3c98bb6b06a4277fc68cbc934
William Ballenthin <william.ballenthin@fireeye.com>
2018-03-25 03:55:46 -0600

    add setup.py

---
+++ setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+import setuptools
+
+setuptools.setup(
+        name="python-vb",
+        version="0.1",
+        description="Pure Python parser for Visual Basic PE files.",
+        long_description="Pure Python parser for Visual Basic PE files.",
+        author="Willi Ballenthin",
+        author_email="willi.ballenthin@gmail.com",
+        url="https://github.com/williballenthin/python-vb",
+        license="Apache 2.0 License",
+        packages=setuptools.find_packages(),
+        install_requires=[
+            'pefile',
+            'hexdump',
+            'argparse',
+            'capstone',
+            'keystone-engine',
+            'unicorn',
+            'six',
+            'pytest',
+            'hexdump',
+            'pytest-cov',
+            ],
+        scripts=[
+                 'scripts/vb_info.py',
+                 ],
+)
